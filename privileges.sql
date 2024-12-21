78d8d6c364149195cfdc841fb5d838b0125aabb5
aariisto <115368147+aariisto@users.noreply.github.com>
2024-12-21 06:16:11 +0100

    Add files via upload

---
+++ privileges.sql
@@ -0,0 +1,20 @@
+-- Créer un utilisateur nommé 'user_velib' avec un mot de passe 'saevelib'
+CREATE USER 'user_velib'@'localhost' IDENTIFIED BY 'saevelib';
+
+-- privilèges sur la table stations (uniquement SELECT)
+GRANT SELECT ON sae_velib.stations TO 'user_velib'@'localhost';
+
+-- privilèges sur la table velo (uniquement SELECT)
+GRANT SELECT ON sae_velib.velo TO 'user_velib'@'localhost';
+
+-- privilèges sur la table reservations (SELECT, INSERT)
+GRANT SELECT, INSERT ON sae_velib.reservations TO 'user_velib'@'localhost';
+
+-- privilèges sur la table recherches (SELECT, DELETE, INSERT)
+GRANT SELECT, DELETE, INSERT ON sae_velib.recherches TO 'user_velib'@'localhost';
+
+-- privilèges sur la table users (SELECT, UPDATE, INSERT)
+GRANT SELECT, UPDATE, INSERT ON sae_velib.users TO 'user_velib'@'localhost';
+
+-- Appliquer les modifications
+FLUSH PRIVILEGES;
